7b8b243b2d7433b0ad2b674b8ab3458c6aa96ae4
adel chouchane <chouchenadel3@gmail.com>
2024-05-14 22:12:35 +0100

    wip

---
+++ coffee.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 14, 2024 at 11:11 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `coffee`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `menuId` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`menuId`, `name`) VALUES
+(1, 'Patisserie'),
+(2, 'Petite déjeuner tout');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menuitem`
+--
+
+CREATE TABLE `menuitem` (
+  `itemId` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `description` text NOT NULL,
+  `price` float NOT NULL,
+  `menuId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `menuitem`
+--
+
+INSERT INTO `menuitem` (`itemId`, `name`, `description`, `price`, `menuId`) VALUES
+(1, 'Croissant (Chocolat)', 'Le croissant nature allie simplicité et élégance,\r\ntandisque le croissant au chocolat offre une\r\nexplosion de saveurs gourmandes avec ses pépites\r\nfondantes pour accompagner votre café', 6, 1),
+(2, 'Croissant (Nature)', 'Le croissant nature allie simplicité et\r\n élégance, tandisque le croissant au\r\n chocolat offre une explosion \r\nde saveurs gourmandes avec ses \r\npépites fondantes pour \r\naccompagner votre café.', 6, 1),
+(3, 'Croissant', 'Le croissant nature allie simplicité et élégance, tandisque le croissant au chocolat offre une explosion de saveurs gourmandes avec ses pépites fondantes pour accompagner votre café.', 6, 2),
+(4, 'Croissant (Chocolat)', 'Le croissant nature allie simplicité et élégance,\r\ntandisque le croissant au chocolat offre une\r\nexplosion de saveurs gourmandes avec ses pépites\r\nfondantes pour accompagner votre café', 6, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `password`) VALUES
+('admin', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`menuId`);
+
+--
+-- Indexes for table `menuitem`
+--
+ALTER TABLE `menuitem`
+  ADD PRIMARY KEY (`itemId`),
+  ADD KEY `fk_menu` (`menuId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `menuId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `menuitem`
+--
+ALTER TABLE `menuitem`
+  MODIFY `itemId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `menuitem`
+--
+ALTER TABLE `menuitem`
+  ADD CONSTRAINT `fk_menu` FOREIGN KEY (`menuId`) REFERENCES `menu` (`menuId`) ON DELETE SET NULL;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
